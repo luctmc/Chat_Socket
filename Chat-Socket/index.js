7f06495e2475b108ee49534a858c7aa0440efaa2
Lucas T. M. Castro <134396081+luctmc@users.noreply.github.com>
2025-06-10 08:16:28 -0300

    Add files via upload

---
+++ Chat-Socket/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const socketIo = require('socket.io');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado!');
+
+    socket.on('join room', (room) => {
+        socket.join(room);
+        console.log(`Usuário entrou na sala: ${room}`);
+    });
+
+    socket.on('leave room', (room) => {
+        socket.leave(room);
+        console.log(`Usuário saiu da sala: ${room}`);
+    });
+
+    socket.on('chat message', (data) => {
+        const { room, user, message, time } = data;
+        io.to(room).emit('chat message', { user, message, time });
+        console.log(`[${room}] ${user}: ${message}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
+});
